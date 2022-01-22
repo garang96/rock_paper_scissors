6671414ed9b101ab4a140e48a1fb5f0c25920096
aliergarang <aliergarang16@gmail.com>
2022-01-23 00:04:45 +0300

    manipulate DOM & create undefined variables& array

---
+++ r-p-s.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll('button');
+const computerChoice = document.getElementById('computer-choice');
+const playerChoice = document.getElementById('player-choice');
+const showResults = document.getElementById('results');
+
+let computerSelection;
+let playerSelection;
+let results;
+
+const choices = ['rock', 'paper', 'scissors'];
