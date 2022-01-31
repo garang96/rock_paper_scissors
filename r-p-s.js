816a5a3e5780f65156e7b210cd37021442a7dd87
aliergarang <aliergarang16@gmail.com>
2022-01-31 10:48:13 +0300

    Include scores and instructions of the game

--- r-p-s.js
+++ r-p-s.js
@@ -2,18 +2,18 @@ const buttons = document.querySelectorAll('button');
 const computerChoice = document.getElementById('computer-choice');
 const playerChoice = document.getElementById('player-choice');
 const showResults = document.getElementById('results');
+const computerPoints = document.getElementById('computer-score');
+const playerPoints = document.getElementById('player-score');
 
 let computerSelection;
 let playerSelection;
 let results;
-let isClicked;
+let computerScore = 0;
+let yourScore = 0;
 
 const choices = ['rock', 'paper', 'scissors'];
 
 const playGame = (event) => {
-    computerChoice.style.display = 'block';
-    playerChoice.style.display = 'block';
-    showResults.style.display = 'block';
     playerSelection = event.target.innerHTML;
     playerChoice.innerHTML = 'Your Choice:' + " " + playerSelection;
     computerPlay();
@@ -21,7 +21,7 @@ const playGame = (event) => {
 }
 
 for (const button of buttons) {
-    isClicked = button.addEventListener('click', playGame);
+    button.addEventListener('click', playGame);
 }
 
 const computerPlay = () => {
@@ -33,18 +33,46 @@ const computerPlay = () => {
 const finalResults = () => {
     if (computerSelection === playerSelection) {
         results = `It's a tie!`;
+        computerScore = 0;
+        yourScore = 0;
+        computerPoints.innerText = `Computer Score: ${computerScore}`;
+        playerPoints.innerText = `Your Score: ${yourScore}`;
     } else if (computerSelection === 'rock' && playerSelection === 'paper') {
-        results = 'You win!';
+        results = 'You win! Paper beats Rock';
+        computerScore = 0;
+        yourScore = 5;
+        computerPoints.innerText = `Computer Score: ${computerScore}`;
+        playerPoints.innerText = `Your Score: ${yourScore}`;
     } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
-        results = 'You lose!';
+        results = 'You lose! Rock beats Scissors';
+        computerScore = 5;
+        yourScore = 0;
+        computerPoints.innerText = `Computer Score: ${computerScore}`;
+        playerPoints.innerText = `Your Score: ${yourScore}`;
     } else if (computerSelection === 'paper' && playerSelection === 'rock') {
-        results = 'You lose!';
+        results = 'You lose! Paper beats Rock';
+        computerScore = 5;
+        yourScore = 0;
+        computerPoints.innerText = `Computer Score: ${computerScore}`;
+        playerPoints.innerText = `Your Score: ${yourScore}`;
     } else if (computerSelection === 'paper' && playerSelection === 'scissors') {
-        results = 'You win'
+        results = 'You win! Scissors beats Paper'
+        computerScore = 0;
+        yourScore = 5;
+        computerPoints.innerText = `Computer Score: ${computerScore}`;
+        playerPoints.innerText = `Your Score: ${yourScore}`;
     } else if (computerSelection === 'scissors' && playerSelection === 'rock') {
-        results = 'You win!';
+        results = 'You win! Rock beats Scissors';
+        computerScore = 0;
+        yourScore = 5;
+        computerPoints.innerText = `Computer Score: ${computerScore}`;
+        playerPoints.innerText = `Your Score: ${yourScore}`;
     } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
-        results = 'You lose!';
+        results = 'You lose! Scissors beats Paper';
+        computerScore = 5;
+        yourScore = 0;
+        computerPoints.innerText = `Computer Score: ${computerScore}`;
+        playerPoints.innerText = `Your Score: ${yourScore}`;
     }
-    showResults.innerHTML = 'Results:' + " " + results;
+    showResults.innerHTML = `Results: ${results}`;
 }
